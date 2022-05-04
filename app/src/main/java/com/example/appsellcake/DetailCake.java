f7385a06a3a5cae275a254096acedce188997a14
thanhjmg <thanhjmg12a3@gmail.com>
2022-05-04 19:59:16 +0700

    giao diện chi tiết

---
+++ app/src/main/java/com/example/appsellcake/DetailCake.java
@@ -0,0 +1,14 @@
+package com.example.appsellcake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailCake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_cake);
+    }
+}
---
+++ app/src/main/java/com/example/appsellcake/entity/Cake.java
@@ -0,0 +1,47 @@
+package com.example.appsellcake.entity;
+
+public class Cake {
+    private int imgDetail;
+    private double price;
+    private String name;
+    private String detail;
+
+    public Cake(int imgDetail, double price, String name, String detail) {
+        this.imgDetail = imgDetail;
+        this.price = price;
+        this.name = name;
+        this.detail = detail;
+    }
+
+    public int getImgDetail() {
+        return imgDetail;
+    }
+
+    public void setImgDetail(int imgDetail) {
+        this.imgDetail = imgDetail;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
