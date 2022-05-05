42d189acfd1d6bac636502154c6aec4647ee472c
Admins <vothanhnho8005@gmail.com>
2022-05-05 14:17:21 +0700

    feature/login_register

--- app/src/main/java/com/example/appsellcake/gui/MainActivity.java
+++ app/src/main/java/com/example/appsellcake/gui/MainActivity.java
@@ -2,15 +2,42 @@ package com.example.appsellcake.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.appsellcake.R;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseDatabase db;
+    private   Button btnregister;
+    private  TextView btnsignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnsignin= findViewById(R.id.main_login);
+        btnregister= findViewById(R.id.main_register);
+
+
+        btnsignin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,loginActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnregister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, registerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
