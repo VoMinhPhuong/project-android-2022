620ffa4416e577e58f13e990637fe124fdb401f6
thanhjmg <thanhjmg12a3@gmail.com>
2022-05-14 20:22:58 +0700

    thiết kế giỏ hàng

---
+++ app/src/main/java/com/example/appsellcake/gui/GioHang.java
@@ -0,0 +1,16 @@
+package com.example.appsellcake.gui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appsellcake.R;
+
+public class GioHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gio_hang);
+    }
+}
